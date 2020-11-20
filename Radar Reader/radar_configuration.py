4208755108fa384dcb62fafa012eb389887e5bce
Ahmed Mahmoud <ahmed.fattah98@eng-st.cu.edu.eg>
2020-11-20 23:50:28 +0200

    fix delayed response in the gui

--- Radar Reader/radar_configuration.py
+++ Radar Reader/radar_configuration.py
@@ -2,33 +2,53 @@ import serial
 import time
 
 class Radar():
+    '''
+    this is the radar configuration class this will implement all low level communication with the radar sensor
+    '''
     def __init__(self,port='com4',baudrate = 1000000):
+        '''
+        define a connection through the serial port
+        '''
         self.ser=serial.Serial(port=port,baudrate=baudrate)
-
     def is_open(self):
-        return self.ser.is_open
-    
+        '''
+            check if the connection is open
+        '''
+        return self.ser.is_open 
     def start(self):
+        '''
+            start the connection
+        '''
         self.ser.open()
 
     def close(self):
+        '''
+            close the connection 
+        '''
         self.ser.close()
     
-    def read_magnitude(self):        
-        line = self.ser.readline() 
+    def clear_buffer(self):
+        '''
+            clear buffer of the pc of the communication
+        '''
+        self.ser.read_all()
+
+   
+
+    def read_magnitude(self,write_file=None):     
+        '''
+        this function for reading a frame from  the radar that contains the magintude information
+        '''   
+        line = self.ser.readline() #read a line from the sensor
         newLine = line.decode("utf-8")        
         splittedLine = newLine.split("\t") 
-        if(splittedLine[0]=='!R'):   
-            #print(splittedLine[1:min(len(splittedLine),6)])
-            pass
-        else:
-            return None
+        if(splittedLine[0]!='!R'):   #check for start frame
+            return None            
         index = -1
         try:
-            index=splittedLine.index('\r\n')
+            index=splittedLine.index('\r\n') #seach for the end frame
         except ValueError as e:
-            print(e)
-            
+            print(e)            
         if(index==-1):
             return None
         else:
@@ -36,14 +56,6 @@ class Radar():
             size=splittedLine[2]
             #print('counter value = '+str(counter))
             #print('size value = '+str(size))
-            return splittedLine[4:index]
+            frame=[int(i) for i in splittedLine[4:index]]       #get the frame  
+            return frame
         return None      
-
-
-
-
-
-    
-	
-        
-	
--- Radar Reader/radar_webUI.py
+++ Radar Reader/radar_webUI.py
@@ -1,65 +1,91 @@
-import dash 
+import dash
 from dash.dependencies import Output, Input
-import dash_core_components as dcc 
-import dash_html_components as html 
-import plotly 
-import random 
-import plotly.graph_objs as go 
-from collections import deque 
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
 from radar_configuration import Radar
 import numpy as np
 import time
-def get_readings(q):
-	radar=Radar(port='com4')
+from radar_signal_processeing import naive_face_detection
+from queue import Queue 
+from threading import Thread
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
+def get_readings():
+	global last_reading
+	'''
+	this function will be an opned thread to take readings from the radar
+	it will get frames from the radar then produce it in the queue
+	'''
+	radar = Radar(port='com4')
 	if(radar.is_open()):
 		radar.close()
-	radar.start()
-	count=1
+	radar.start()	
+	radar.clear_buffer()
 	while 1:
-		reading=radar.read_magnitude()
-		count+=1		
+		reading = radar.read_magnitude()		
 		if reading is not None:
-			if q.full():
-				q.queue.clear()
-			q.put(reading)
+			last_reading=reading
 
 
+app = dash.Dash(__name__)
 
+app.layout = html.Div(
+	[
+		html.H4('RADAR FACE IDENTIFICATION'),
+        html.Div(id='live-update-text'),
+		dcc.Graph(id='live-graph', animate=False),
+		dcc.Interval(
+			id='graph-update',
+			interval=100,
+			n_intervals=0
+		),
+	]
+)
+
+
+@app.callback(Output('live-update-text', 'children'),
+              Input('graph-update', 'n_intervals'))
+def update_metrics(n):
+	style = {'padding':'5px','fontsize':'16px'}
+	threashold=-20
+	Y=None
+	if last_reading is not None:
+		Y=last_reading
+	else:
+		return[
+		html.Span("Hello",style=style)
+	]
+
+	distance, magnitude = naive_face_detection(Y)
+	if(magnitude is None):
+		return[
+		html.Span("Hello",style=style)
+	]
+	text = ""
+	if magnitude < threashold:
+		text = 'there is no face sorry'
+	else:
+		text = 'there is a face at distance ' + str(distance)
 	
-
-app = dash.Dash(__name__) 
-
-app.layout = html.Div( 
-	[ 
-		dcc.Graph(id = 'live-graph', animate = True), 
-		dcc.Interval( 
-			id = 'graph-update', 
-			interval = 100, 
-			n_intervals = 0
-		), 
-	] 
-) 
+	return[
+		html.Span(text,style=style)
+	]
+    
 
 @app.callback( 
 	Output('live-graph', 'figure'), 
 	[ Input('graph-update', 'n_intervals') ] 
 ) 
-
-
 def update_graph_scatter(n): 
-	global X,Y,q	
-	if not q.empty():
-		Y_new=q.get()
-		print('hellooo')	
-		print(len(Y_new))	
-		if(len(Y_new)==255):	
-			print(Y_new[0])		
-			Y=Y_new	
-			print(len(Y))
-			print(len(X))
-		else:			
-			Y=np.zeros(len(X))	
-	print('yessssss')
+	global X,Y,last_reading
+	print(last_reading)
+	if last_reading is not None:
+		Y=last_reading
 	data = plotly.graph_objs.Scatter( 
 			x=list(X), 
 			y=list(Y), 
@@ -68,21 +94,17 @@ def update_graph_scatter(n):
 	) 
 
 	return {'data': [data], 
-			'layout' :go.Layout(xaxis=dict(range=[min(X),max(X)]),yaxis = dict(range = [-140,0]),)			
+			'layout' :go.Layout(xaxis=dict(range=[min(X),max(X)]),yaxis = dict(range = [-160,20]))			
 			} 
 
-
-from queue import Queue 
-from threading import Thread
 
 
 
 if __name__=='__main__':
 	X=[]
 	X=np.linspace(0,8268.8,255)
-	Y=[]
-	q = Queue(50)	
-	t1 = Thread(target = get_readings, args =(q, )) 
+	Y=[]	
+	last_reading=None
+	t1 = Thread(target = get_readings,daemon=True) 
 	t1.start()
 	app.run_server()
-	
